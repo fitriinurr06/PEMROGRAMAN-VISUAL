c44cab3fead39afce5e84fa66cc27b934ac02c84
fitriinurr06 <fn158323@gmail.com>
2022-01-18 19:02:29 +0700

    push pertemuan 13

---
+++ docs/Pertemuan_13/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TokoBungaFlorist
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btncetak = New System.Windows.Forms.Button()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btncetak
+        '
+        Me.btncetak.Location = New System.Drawing.Point(50, 217)
+        Me.btncetak.Name = "btncetak"
+        Me.btncetak.Size = New System.Drawing.Size(75, 23)
+        Me.btncetak.TabIndex = 0
+        Me.btncetak.Text = "Cetak Struk"
+        Me.btncetak.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.DataGridView1.Location = New System.Drawing.Point(31, 31)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(425, 164)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Nama Bunga"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Jumlah"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Harga"
+        Me.Column3.Name = "Column3"
+        '
+        'TokoBungaFlorist
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(622, 266)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btncetak)
+        Me.Name = "TokoBungaFlorist"
+        Me.Text = "Toko Bunga ""Florist"""
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btncetak As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+End Class
---
+++ docs/Pertemuan_13/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Drawing.Printing
+Public Class TokoBungaFlorist
+    Dim WithEvents PD As New PrintDocument
+    Dim PPD As New PrintPreviewDialog
+    Dim t_harga As Long
+    Dim t_jml As Long
+    Private Sub btncetak_Click(sender As Object, e As EventArgs) Handles btncetak.Click
+        PPD.Document = PD
+        PPD.ShowDialog()
+        'PD.Print()
+
+    End Sub
+
+    Private Sub TokoBungaFlorist_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PD_PrintPage(sender As Object, e As PrintPageEventArgs) Handles PD.PrintPage
+        Dim f10 As New Font("Times New Roman", 10, FontStyle.Regular)
+        Dim f10b As New Font("Times New Roman", 10, FontStyle.Bold)
+        Dim f14 As New Font("Times New Roman", 14, FontStyle.Bold)
+
+        Dim leftmargin As Integer = PD.DefaultPageSettings.Margins.Left
+        Dim centermargin As Integer = PD.DefaultPageSettings.PaperSize.Width / 2
+        Dim rightmargin As Integer = PD.DefaultPageSettings.PaperSize.Width
+
+        Dim kanan As New StringFormat
+        Dim tengah As New StringFormat
+        kanan.Alignment = StringAlignment.Far
+        tengah.Alignment = StringAlignment.Center
+
+        Dim garis As String
+        garis = "-------------------------------------------------------------------------"
+
+        e.Graphics.DrawString("Toko Bunga Florist", f14, Brushes.Black, centermargin, 5, tengah)
+        e.Graphics.DrawString("Jln.Cengkeh No.14 Taman Sari Jakarta Barat", f10, Brushes.Black, centermargin, 25, tengah)
+        e.Graphics.DrawString("No.HP 087-990-889-007", f10, Brushes.Black, centermargin, 40, tengah)
+
+        e.Graphics.DrawString("No.Faktur", f10, Brushes.Black, 0, 60)
+        e.Graphics.DrawString(":", f10, Brushes.Black, 65, 60)
+        e.Graphics.DrawString("12345", f10, Brushes.Black, 75, 60)
+
+        e.Graphics.DrawString("Kasir", f10, Brushes.Black, 0, 75)
+        e.Graphics.DrawString(":", f10, Brushes.Black, 65, 75)
+        e.Graphics.DrawString("Alyssa", f10, Brushes.Black, 75, 75)
+
+        e.Graphics.DrawString("26/07/2021 00:00", f10, Brushes.Black, 0, 90)
+
+        e.Graphics.DrawString(garis, f10, Brushes.Black, 0, 100)
+        DataGridView1.AllowUserToAddRows = False
+
+        Dim tinggi As Integer
+        Dim i As Long
+        For baris As Integer = 0 To DataGridView1.RowCount - 1
+            tinggi += 15
+            e.Graphics.DrawString(DataGridView1.Rows(baris).Cells(1).Value.ToString, f10, Brushes.Black, 0, 100 + tinggi)
+            e.Graphics.DrawString(DataGridView1.Rows(baris).Cells(0).Value.ToString, f10, Brushes.Black, 50, 100 + tinggi)
+
+            i = DataGridView1.Rows(baris).Cells(2).Value
+            DataGridView1.Rows(baris).Cells(2).Value = Format(i, "##,##0")
+            e.Graphics.DrawString(DataGridView1.Rows(baris).Cells(2).Value.ToString, f10, Brushes.Black, rightmargin, 100 + tinggi, kanan)
+
+        Next
+        tinggi = 110 + tinggi
+        hitungtotal()
+
+        e.Graphics.DrawString(garis, f10, Brushes.Black, 0, tinggi)
+        e.Graphics.DrawString("Total Harga : " & Format(t_harga, "##,##0"), f10b, Brushes.Black, rightmargin, 10 + tinggi, kanan)
+        e.Graphics.DrawString(t_jml, f10b, Brushes.Black, 0, 10 + tinggi)
+
+        e.Graphics.DrawString("Terimakasih Telah Belanja", f10, Brushes.Black, centermargin, 35 + tinggi, tengah)
+        e.Graphics.DrawString("Jangan Lupa Mampir Lagi Ya :)", f10, Brushes.Black, centermargin, 50 + tinggi, tengah)
+    End Sub
+
+    Sub hitungtotal()
+        Dim hitung As Long = 0
+        For baris As Long = 0 To DataGridView1.RowCount - 1
+            hitung = hitung + DataGridView1.Rows(baris).Cells(2).Value
+        Next
+        t_harga = hitung
+
+        Dim jml As Long = 0
+        For baris As Long = 0 To DataGridView1.RowCount - 1
+            jml = jml + DataGridView1.Rows(baris).Cells(1).Value
+        Next
+        t_jml = jml
+    End Sub
+
+    Private Sub PD_BeginPrint(sender As Object, e As PrintEventArgs) Handles PD.BeginPrint
+        Dim pagesetup As New PageSettings
+        pagesetup.PaperSize = New PaperSize("Custom", 350, 400)
+        PD.DefaultPageSettings = pagesetup
+    End Sub
+End Class
